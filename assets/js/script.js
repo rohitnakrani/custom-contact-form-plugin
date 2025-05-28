0fa24e5f93e90d6088d3aa70b9139bca6d67071e
Rohit Nakrani <59042105+rohitnakrani@users.noreply.github.com>
2025-05-28 18:14:32 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-
+jQuery(document).ready(function($) {
+    $('#ccfp-contact-form').on('submit', function(e) {
+        e.preventDefault();
+        var form = $(this);
+        var data = form.serialize();
+        $.post(ccfp_ajax_obj.ajax_url, data, function(response) {
+            if (response.success) {
+                $('#ccfp-response').html(response.data).css('color', 'green');
+                form[0].reset();
+            } else {
+                $('#ccfp-response').html(response.data).css('color', 'red');
+            }
+        });
+    });
+});
